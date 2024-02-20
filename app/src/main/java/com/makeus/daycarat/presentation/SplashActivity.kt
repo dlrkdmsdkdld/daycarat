e77d68ee81f34e59d1d993e18e1f3b1809c74494
이가형 <dlrkdmsdkdld@naver.com>
2024-02-20 19:18:55 +0900

    fix : QA 진행중

--- app/src/main/java/com/makeus/daycarat/presentation/SplashActivity.kt
+++ app/src/main/java/com/makeus/daycarat/presentation/SplashActivity.kt
@@ -5,10 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.makeus.daycarat.R
-import com.makeus.daycarat.presentation.login.AgreementActivity
-import com.makeus.daycarat.presentation.login.IntroduceActivity
-import com.makeus.daycarat.presentation.login.JoinActivity
-import com.makeus.daycarat.presentation.login.LoginActivity
 import com.makeus.daycarat.util.Constant
 import com.makeus.daycarat.util.SharedPreferenceManager
 
@@ -18,16 +14,9 @@ class SplashActivity : AppCompatActivity() {
         val splashScreen = installSplashScreen()
         setContentView(R.layout.activity_splash)
 //        SharedPreferenceManager.getInstance().setString(Constant.USER_ACCESS_TOKEN,"eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJtdXNocm9vbTEzMjRAbmF2ZXIuY29tIiwicm9sZSI6IlVTRVIiLCJpYXQiOjE3MDU0MTE5MTAsImV4cCI6MTcxNDA1MTkxMH0._qYlVOQKnYwQtUXetaVRftc0E4BJZ99-r9iu6kztdv4")
-        if (SharedPreferenceManager.getInstance().getString(Constant.USER_ACCESS_TOKEN,"").isEmpty()){
-            Intent(this, LoginActivity::class.java).apply {
-                startActivity(this)
-                finish()
-            }
-        }else{
-            Intent(this, MainActivity::class.java).apply {
-                startActivity(this)
-                finish()
-            }
+        Intent(this, MainActivity::class.java).apply {
+            startActivity(this)
+            finish()
         }
 
 
