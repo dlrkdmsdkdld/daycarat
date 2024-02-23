2c7e4b4b5b6d60029962ed46c80c7f43445b5afe
이가형 <dlrkdmsdkdld@naver.com>
2024-02-24 00:17:22 +0900

    release : 0.1.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.makeus.daycarat"
         minSdk = 24
         targetSdk = 33
-        versionCode = 11
-        versionName = "0.1.1"
+        versionCode = 16
+        versionName = "0.1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
